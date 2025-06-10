f2635b2a60d2cb2539f5e06c110f7a3a2b1c55be
Gabriel Alexandre <gabrielxpaz@gmail.com>
2025-06-10 19:44:12 -0300

    correções de rota post e porta

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.status(200).send('Hello, World!');
 });
 
 app.post("/api/salvar", (req, res) =>{
     const data = req.body;
     console.log("Dados recebidos:", data);
+    res.status(200).json({
+      message: "Dados recebidos com sucesso!",
+      data: data
+    })
 })
 
 app.listen(PORT, () => {
